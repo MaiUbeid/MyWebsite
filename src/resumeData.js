49ef56ccc0a124b4bb8e3eb99619c729020fd8fd
MaiUbeid <ubead95@gmail.com>
2020-02-18 12:38:00 +0200

    add files for website

---
+++ src/resumeData.js
@@ -0,0 +1,114 @@
+let resumeData = {
+  imagebaseurl: 'https://i.imgur.com/EKORinc.jpg',
+  name: 'Mai R. Ubeid',
+  role: 'Software Engineer',
+  linkedinId: 'maiubeid',
+  skypeid: 'MaiUbeid',
+  roleDescription:
+    'Passionate and creative software engineer working on front-end web development, developed responsive, clean code and up to date web applications using agile methodology with many international clients.',
+  socialLinks: [
+    {
+      name: 'linkedin',
+      url: 'https://www.linkedin.com/in/maiubeid/',
+      className: 'fa fa-linkedin'
+    },
+    {
+      name: 'github',
+      url: 'http://github.com/MaiUbeid/',
+      className: 'fa fa-github'
+    },
+    {
+      name: 'skype',
+      url: 'http://twitter.com/MaiUbeid/',
+      className: 'fa fa-twitter'
+    }
+  ],
+  aboutme:
+    'Passionate and creative software engineer working on front end web development, developed responsive, clean code and up to date web applications using agile methodology with many international clients. Offering skills in debugging programs to real problems. Also, I work on adding value to the business, seeking a position with a top technology ﬁrm.',
+  address: 'Gaza, Palestine',
+  website: '',
+  education: [
+    {
+      UniversityName: 'The LNM Insitute of Information Technology',
+      specialization: 'Some specialization',
+      MonthOfPassing: 'Aug',
+      YearOfPassing: '2020',
+      Achievements: 'Some Achievements'
+    },
+    {
+      UniversityName: 'Some University',
+      specialization: 'Some specialization',
+      MonthOfPassing: 'Jan',
+      YearOfPassing: '2018',
+      Achievements: 'Some Achievements'
+    }
+  ],
+  work: [
+    {
+      CompanyName: 'Some Company',
+      specialization: 'Some specialization',
+      MonthOfLeaving: 'Jan',
+      YearOfLeaving: '2018',
+      Achievements: 'Some Achievements'
+    },
+    {
+      CompanyName: 'Some Company',
+      specialization: 'Some specialization',
+      MonthOfLeaving: 'Jan',
+      YearOfLeaving: '2018',
+      Achievements: 'Some Achievements'
+    }
+  ],
+  skillsDescription: 'Your skills here',
+  skills: [
+    {
+      skillname: 'Reactjs'
+    },
+    {
+      skillname: 'Nodejs'
+    },
+    {
+      skillname: 'React Native'
+    },
+    {
+      skillname: 'HTML5'
+    },
+    {
+      skillname: 'CSS3'
+    }
+  ],
+  portfolio: [
+    {
+      name: 'project1',
+      description: 'mobileapp',
+      imgurl: 'images/portfolio/phone.jpg'
+    },
+    {
+      name: 'project2',
+      description: 'mobileapp',
+      imgurl: 'images/portfolio/project.jpg'
+    },
+    {
+      name: 'project3',
+      description: 'mobileapp',
+      imgurl: 'images/portfolio/project2.png'
+    },
+    {
+      name: 'project4',
+      description: 'mobileapp',
+      imgurl: 'images/portfolio/phone.jpg'
+    }
+  ],
+  testimonials: [
+    {
+      description: 'This is a sample testimonial',
+      name: 'Some technical guy'
+    },
+    {
+      description: 'This is a sample testimonial',
+      name: 'Some technical guy'
+    }
+  ]
+};
+
+export default resumeData;
