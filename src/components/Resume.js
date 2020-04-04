c9e6b3343f14801acc251fd3de3b629ae9f87d3f
MaiUbeid <ubead95@gmail.com>
2020-04-04 16:40:28 +0300

    update my prfile

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -4,6 +4,30 @@ export default class Resume extends Component {
     let resumeData = this.props.resumeData;
     return (
       <section id="resume">
+                <div className="row skill">
+          <div className="three columns header-col">
+            <h1>
+              <span>Skills</span>
+            </h1>
+          </div>
+
+          <div className="nine columns main-col">
+            <p>{resumeData.skillsDescription}</p>
+
+            <div className="bars">
+              <ul className="skills">
+                {resumeData.skills &&
+                  resumeData.skills.map(item => {
+                    return (
+                      <li>
+                        {item.skillname}
+                      </li>
+                    );
+                  })}
+              </ul>
+            </div>
+          </div>
+        </div>
         <div className="row work">
           <div className="three columns header-col">
             <h1>
@@ -45,62 +69,6 @@ export default class Resume extends Component {
           </div>
         </div>
 
-        <div className="row education">
-          <div className="three columns header-col">
-            <h1>
-              <span>Education</span>
-            </h1>
-          </div>
-
-          <div className="nine columns main-col">
-            {resumeData.education &&
-              resumeData.education.map(item => {
-                return (
-                  <div className="row item">
-                    <div className="twelve columns">
-                      <h3>{item.UniversityName}</h3>
-                      <p className="info">
-                        {item.specialization}
-                        <span>&bull;</span>{' '}
-                        <em className="date">
-                          {item.MonthOfPassing} {item.YearOfPassing}
-                        </em>
-                      </p>
-                      <p>{item.Achievements}</p>
-                    </div>
-                  </div>
-                );
-              })}
-          </div>
-        </div>
-
-        <div className="row skill">
-          <div className="three columns header-col">
-            <h1>
-              <span>Skills</span>
-            </h1>
-          </div>
-
-          <div className="nine columns main-col">
-            <p>{resumeData.skillsDescription}</p>
-
-            <div className="bars">
-              <ul className="skills">
-                {resumeData.skills &&
-                  resumeData.skills.map(item => {
-                    return (
-                      <li>
-                        <span
-                          className={`bar-expand ${item.skillname.toLowerCase()}`}
-                        ></span>
-                        <em>{item.skillname}</em>
-                      </li>
-                    );
-                  })}
-              </ul>
-            </div>
-          </div>
-        </div>
       </section>
     );
   }
